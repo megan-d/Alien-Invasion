0af7a98eba8473ee0c9520eaa3c67b1ac8bc8488
Megan D <45672063+megan-d@users.noreply.github.com>
2024-03-08 14:50:30 -0700

    continue on game movement

--- alien.py
+++ alien.py
@@ -4,7 +4,7 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """A class to represent a single alien in the fleet"""
     
-    def __init__(self, ai_settings, screen) -> None:
+    def __init__(self, ai_settings, screen):
         """Initialize the alien and set its starting position"""
         super(Alien, self).__init__()
         self.screen = screen
@@ -24,3 +24,8 @@ class Alien(Sprite):
     def blitme(self):
         """Draw the alien at its current location"""
         self.screen.blit(self.image, self.rect)
+    
+    def update(self):
+        """Move the alien right"""
+        self.x += self.ai_settings.alien_speed_factor
+        self.rect.x = self.x
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 async def main():
@@ -15,19 +14,20 @@ async def main():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     
+    # Make a ship, a group to store bullets in, and a group of aliens
     ship = Ship(screen, ai_settings)
-    # Make a group to store bullets in
     bullets = Group()
+    aliens = Group()
     
-    # Make an alien
-    alien = Alien(ai_settings=ai_settings, screen=screen)
+    # Create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens) 
     
     # Start the main loop for the game
-   
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings=ai_settings, screen=screen, ship=ship, bullets=bullets, alien=alien)
+        gf.update_aliens(aliens)
+        gf.update_screen(ai_settings=ai_settings, screen=screen, ship=ship, bullets=bullets, aliens=aliens)
 
 asyncio.run(main())
